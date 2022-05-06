d1174925a0e26cb81d276bf68d2504edfd509206
Satish Prasad <spd61185@gmail.com>
2022-05-06 17:47:09 +0530

    React project

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+     <h1>Welcome to My Labs!</h1>
+    )
+  }
+}
+
+export default Dashboard;
