a7d9467462141279ceaff280ecd0f138bae37a63
Satish Prasad <spd61185@gmail.com>
2022-05-09 08:06:30 +0530

    Clinet project updated

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import { Component } from 'react';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom"; 
+import AddProject from './components/Project/AddProject';
+
+
 class App extends Component{
   render(){
     return(
+      <Router>
       <div className="App">
+        <Header/>
         <Dashboard/>
+        <Route exact path="/dashborad" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
       </div>
+      </Router>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import CreateProjectButton from './Project/CreateProjectButton';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
-     <h1>Welcome to My Labs!</h1>
-    )
+    <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br/>
+                        <CreateProjectButton/>
+                    <br/>
+                    <hr/>
+                   <ProjectItem/>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
   }
 }
 
