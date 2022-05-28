3ca92c96b57494f76c97f1a78e16386981e09b38
Satish Prasad <spd61185@gmail.com>
2022-05-28 14:46:24 +0530

    Update projects

--- springboot-testcontainers/src/main/java/com/satishlabs/springboot/repository/StudentRepository.java
+++ springboot-testcontainers/src/main/java/com/satishlabs/springboot/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.satishlabs.springboot.repository;
 
 import com.satishlabs.springboot.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 }
--- springboot-testcontainers/src/test/java/com/satishlabs/springboot/SpringbootTestcontainersApplicationTests.java
+++ springboot-testcontainers/src/test/java/com/satishlabs/springboot/SpringbootTestcontainersApplicationTests.java
@@ -1,13 +1,46 @@
 package com.satishlabs.springboot;
 
+import com.satishlabs.springboot.entity.Student;
+import com.satishlabs.springboot.repository.StudentRepository;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringbootTestcontainersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
+    //  given/when/then format - BDD Style
+    @Test
+    public void givenStudent_whenGetAllStudents_thenListOfStudents() throws Exception {
+        //given - setup or precondtion
+        List<Student> students =
+                List.of(Student.builder().firstName("Satish").lastName("Prasad").email("sat@gmail").build(),
+                        Student.builder().firstName("Test").lastName("sprinboot").email("test@gmail").build());
+        studentRepository.saveAll(students);
+        //when - action
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/students"));
+
+        //then - verif the output
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(students.size())));
+    }
 }
